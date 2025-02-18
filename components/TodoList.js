e4bef9b8049b1fcf15cdd12b63c5a549f5cfc015
Reima Riihimäki <reimarii@oamk.fi>
2025-02-18 22:11:55 +0200

    Converted to use paper accordion

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,49 +1,32 @@
-import { FlatList, ScrollView, StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet } from "react-native";
 import TodoItem from "./TodoItem";
-import { IconButton, MD3DarkTheme, MD3LightTheme, Text } from "react-native-paper";
+import { List, MD3LightTheme } from "react-native-paper";
 import { useState } from "react";
 
-export function TodoList({todos}){
-    const [uncheckOpen, setUncheckOpen] = useState(false);
-    const [checkOpen, setCheckOpen] = useState(false);
-
+export function TodoList({ todos }) {
     const doneTodos = [];
     const undoneTodos = [];
 
-    todos.forEach( t => t.done ? doneTodos.push(t) : undoneTodos.push(t) );
+    todos.forEach(t => t.done ? doneTodos.push(t) : undoneTodos.push(t));
 
-
-    return(
+    return (
         <ScrollView>
-            <View style={styles.header}>
-                <Text variant="titleMedium">Unchecked ({undoneTodos.length})</Text>
-                <IconButton 
-                    icon={ uncheckOpen ? 'arrow-up-bold-box' : 'arrow-down-bold-box'}
-                    iconColor={MD3LightTheme.colors.primary}
-                    onPress={()=>setUncheckOpen(prev => !prev)}
-                />
-            </View>
-            { uncheckOpen &&
-                undoneTodos.map(t => <TodoItem key={t.id} todoItem={t}/>)
-            }
-            <View style={styles.header}>
-                <Text variant="titleMedium">Checked ({doneTodos.length})</Text>
-                <IconButton 
-                    icon={ checkOpen ? 'arrow-up-bold-box' : 'arrow-down-bold-box'}
-                    iconColor={MD3LightTheme.colors.primary}
-                    onPress={()=>setCheckOpen(prev => !prev)}
-                />
-            </View>
-            { checkOpen &&
-                doneTodos.map(t => <TodoItem key={t.id} todoItem={t}/>)
-            }
+            <List.Accordion style={styles.accordion} titleStyle={styles.accordionTitle} title={`Unchecked (${undoneTodos.length})`} >
+                {undoneTodos.map(t => <TodoItem key={t.id} todoItem={t} />)}
+            </List.Accordion>
+            <List.Accordion style={styles.accordion} titleStyle={styles.accordionTitle} title={`Checked (${doneTodos.length})`} >
+                {doneTodos.map(t => <TodoItem key={t.id} todoItem={t} />)}
+            </List.Accordion>
         </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
-   header:{
-    flexDirection: 'row',
-    alignItems: 'center'
-   }
+    accordion:{
+        borderBottomWidth: 1,
+        borderColor: MD3LightTheme.colors.primary
+    },
+    accordionTitle: {
+       fontSize: 22
+    }
 })
