10e03eef7c6606126b8d00c59621da386c7e9160
Reima Riihimäki <reimarii@oamk.fi>
2025-02-19 08:56:21 +0200

    Auth configuration

--- firebase/Config.js
+++ firebase/Config.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_API_KEY,
@@ -14,4 +16,10 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+
 export const TODOS_REF = 'todos';
+export const USERS_REF = 'users';
+
+export const auth = initializeAuth(app,{
+  persistence: getReactNativePersistence(AsyncStorage)
+});
