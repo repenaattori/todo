fbfeb8d4c4269d3600524e229d33ad0e95c924ea
Reima Riihimäki <reimarii@oamk.fi>
2025-02-19 10:42:24 +0200

    Added nav packages

--- App.js
+++ App.js
@@ -1,13 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { Alert, FlatList, StyleSheet, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { use, useEffect, useState } from 'react';
-import { Button, Divider, MD2LightTheme, MD3LightTheme, PaperProvider, Text, TextInput } from 'react-native-paper';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { addTodo, removeAllTodos, useFireTodos } from './firebase/FirestoreController';
-import TodoItem from './components/TodoItem';
-import { TodoList } from './components/TodoList';
-import { logoutUser, signUpUser, useFireAuth } from './firebase/FirebaseAuthConroller';
+import { StyleSheet } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import { useFireAuth } from './firebase/FirebaseAuthConroller';
 import Login from './screens/Login';
 import Todos from './screens/Todos';
 
